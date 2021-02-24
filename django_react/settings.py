5d4ba81af6c0185067e626903eb60ed1ccd1e261
dmjang <dogzz9445@gmail.com>
2021-02-24 09:21:15 +0900

    Modify secret key setting

--- django_react/settings.py
+++ django_react/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_react.settings
+from django_react.secret import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tzfamilyreact.duckdns.org',]
+ALLOWED_HOSTS = ['tzfamilyreact.duckdns.org', '127.0.0.1']
 
 
 # Application definition
