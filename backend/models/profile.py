8a6584f73f6b5eb91058e5d0cc1e412769b16aa0
dmjang <dogzz9445@gmail.com>
2021-03-05 18:01:59 +0900

    Add profile that abstract the auth.user of Django, also initialize test components for testing models and views

---
+++ backend/models/profile.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+# from django.contrib.auth.models import User as DjangoUser
+from django.contrib.auth.models import User
+
+# class Post(DjangoUser):
+
+#     # Metadata
+#     class Meta:
+#         ordering = ['id']
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_pk = models.IntegerField(blank=True)
+    nickname = models.CharField(max_length=200, blank=True)
+    point = models.IntegerField(default=0)
+    phone = models.CharField(max_length=200, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance, user_pk=instance.id)
+        
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    # Metadata
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        Profile.objects.all()
+        return self.nickname
